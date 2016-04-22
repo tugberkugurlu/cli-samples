4feb7662f27e162aacb04bd3330a97168891376d
Tugberk Ugurlu <tugberk@outlook.com>
2016-04-22 13:32:50 +0100

    inject IHttpContextAccessor into controller

--- HelloMvcApi/Controllers/ProductsController.cs
+++ HelloMvcApi/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMvcApi
@@ -7,6 +8,13 @@ namespace HelloMvcApi
     [Route("/api/products")]
     public class ProductsController
     {
+        private readonly HttpContext _httpContext;
+        
+        public ProductsController(IHttpContextAccessor contextAccessor) 
+        {
+            _httpContext = contextAccessor.HttpContext;
+        }
+        
         private static List<Product> _products = new List<Product>(new[] {
             new Product() { Id = 1, Name = "Computer" },
             new Product() { Id = 2, Name = "Radio" },
